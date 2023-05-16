1b983df2c12139319850466ddf411bd3b94a865c
CopiaPiratA <aleustrsa2004@gmail.com>
2023-05-15 22:01:27 -0300

    Add a delete button and css

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+
+var completeString = []
+var numbersString = []
+
+function clear(){
+    numbersString = [];
+    completeString = []
+    input.value = "";
+}
+buttons.forEach(button => button.addEventListener("click",()=>{
+    completeString.push(button.innerHTML);
+    numbersString = completeString.join("");
+    input.value = numbersString;
+    console.log(numbersString)
+
+        if (numbersString[0] === "+" || numbersString[0]=== "-"
+            || numbersString[0]=== "/" ||numbersString[0]=== "*"){
+                alert("Select a number first")
+                clear()
+            }
+                if(button.innerHTML === "C"){
+                    clear()
+                }
+                    else if(button.innerHTML === "="){
+                        numbersString = numbersString.slice(0,-1);
+                        input.value = numbersString;
+                        input.value = eval(numbersString);
+                        let result = eval(numbersString);
+                        completeString = [result];
+                        return result;}
+                        
+                            else if(button.innerHTML === "←"){
+                                completeString =completeString.slice(0,-1);
+                                completeString =completeString.slice(0,-1);
+                            }
+})) 
